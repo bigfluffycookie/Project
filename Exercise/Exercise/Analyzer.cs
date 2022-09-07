3699905c1d1c4cb41145a30c5f72e5cf568d7711
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-09-07 15:49:17 +0200

    Add basic structure to main program.

---
+++ Exercise/Exercise/Analyzer.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise
+{
+    public class Analyzer
+    {
+        public static void Analyze(string[] lines)
+        {
+            Console.WriteLine("Analyzing");
+        }
+    }
+}
--- Exercise/Exercise/Program.cs
+++ Exercise/Exercise/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,    World!");
+﻿using System.IO;
+
+namespace Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DisplayWelcomeText();
+            string filePath = ReadUserInput();
+            string[] fileContent = GetFileContentFromPath(filePath);
+            Analyzer.Analyze(fileContent);
+
+            Console.Write("Press any key to close App");
+            Console.ReadKey();
+        }
+
+        private static void DisplayWelcomeText()
+        {
+            Console.WriteLine("Welcome to the Analyzer");
+            Console.WriteLine("Please Input Text File path to Analyze");
+        }
+
+        private static string ReadUserInput()
+        {
+            // TODO(leyla.buechel): Check if input is correct,eg exists
+            return Console.ReadLine();
+        }
+
+        private static string[] GetFileContentFromPath(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
