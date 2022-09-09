e921dd4a9d6f46163c8422e819f6eed454d142f7
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-09-09 16:14:51 +0200

    Create Analyzer Unit tests.

---
+++ Exercise/Exercise.UnitTests/AnalyzerTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercise.UnitTests
+{
+    [TestClass]
+    public class AnalyzerTest
+    {
+        [TestMethod]
+        public void Analyze_WithNoLines_ReturnsEmptyList()
+        {
+            string[] lines = new string[0];
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithOneLineNoTodo_ReturnsEmptyList()
+        {
+            string[] lines = new string[] { "" };
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithOneLineWithTodo_ReturnsListWithOneLine()
+        {
+            string[] lines = new string[] { "TODO" };
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithTwoLinesNoTodo_ReturnsEmptyList()
+        {
+            string[] lines = new string[] { "", "" };
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithTwoLinesOneTodo_ReturnsListWithOneLine()
+        {
+            string[] lines = new string[] { "TODO", "" };
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void Analyzer_WithTwoLinesTwoTodo_ReturnsListWithTwoLines()
+        {
+            string[] lines = new string[] { "TODO", "TODO" };
+            List<string> result = Analyzer.Analyze(lines);
+            Assert.AreEqual(2, result.Count);
+        }
+    }
+}
