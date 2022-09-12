e7012cbf5929a9f653553179f9a478a36413b00c
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-09-12 14:39:24 +0200

    Add Object "Issue" that contains line, column and text information Change Test classes name from singular "Test" to plural "Tests" Change some test method names to be more readable Add some more specific tests

--- Exercise/Exercise.UnitTests/InputValidatorTests.cs
+++ Exercise/Exercise.UnitTests/InputValidatorTests.cs
@@ -1,19 +1,19 @@
 namespace Exercise.UnitTests
 {
     [TestClass]
-    public class InputValidatorTest
+    public class InputValidatorTests
     {
         [TestMethod]
-        public void FileHasCorrectExtension_WithWrongExtension_ReturnsFalse()
+        public void FileHasCorrectExtension_WrongExtension_ReturnsFalse()
         {
-            string filePath = System.IO.Directory.GetCurrentDirectory() + @"\Test.txt";
+            string filePath = "Test.txt";
             Console.WriteLine(filePath);
             bool hasCorrectExtension = InputValidator.FileHasCorrectExtension(filePath, ".exr");
             Assert.IsFalse(hasCorrectExtension);
         }
 
         [TestMethod]
-        public void FileHasCorrectExtension_WithCorrectExtension_ReturnsTrue()
+        public void FileHasCorrectExtension_CorrectExtension_ReturnsTrue()
         {
             string filePath =  "Test.txt";
             bool hasCorrectExtension = InputValidator.FileHasCorrectExtension(filePath, ".txt");
@@ -21,7 +21,7 @@ namespace Exercise.UnitTests
         }
 
         [TestMethod]
-        public void FileExists_WithEmptyPath_ReturnsFalse()
+        public void FileExists_EmptyPath_ReturnsFalse()
         {
             string filePath = "";
             bool isValidTextFile = InputValidator.FileExists(filePath);
@@ -29,7 +29,7 @@ namespace Exercise.UnitTests
         }
 
         [TestMethod]
-        public void FileExists_WithCorrectPath_ReturnsTrue()
+        public void FileExists_ExistingPath_ReturnsTrue()
         {
             string filePath = System.IO.Directory.GetCurrentDirectory() + @"\Test.txt";
             System.IO.File.Create(filePath);
--- Exercise/Exercise/Analyzer.cs
+++ Exercise/Exercise/Analyzer.cs
@@ -1,20 +1,18 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Exercise
+﻿namespace Exercise
 {
     public class Analyzer
     {
-        public static List<string> Analyze(string[] lines)
+        public static List<Issue> Analyze(string[] lines)
         {
-            List<string> result = new List<string>();
-            List<string> resultToDo = GetTodoLines(lines);
+            List<Issue> result = new List<Issue>();
+            List<Issue> resultToDo = GetTodoLines(lines);
             result.AddRange(resultToDo);
             return result;
         }
 
-        private static List<string> GetTodoLines(string[] lines)
+        private static List<Issue> GetTodoLines(string[] lines)
         {
-            List<string> result = new List<string>();
+            List<Issue> result = new List<Issue>();
             for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
@@ -23,11 +21,8 @@ namespace Exercise
                 {
                     continue;
                 }
-                string todoLine = "";
-                todoLine += "Line: " + i.ToString() + ", ";
-                todoLine += "Column: " + indexOfTodo.ToString() + ", ";
-                todoLine += "'" + line[indexOfTodo..] + "'";
-                result.Add(todoLine);
+                Issue issue = new Issue(line[indexOfTodo..], i + 1, indexOfTodo + 1);
+                result.Add(issue);
             }
 
             return result;
