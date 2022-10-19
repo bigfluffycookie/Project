867a318fa59259c82bd8c4a8b4d494791410f5d0
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-10-19 12:21:42 +0100

    Modify program to use the new config providers
    At this point we haven't deleted the old interfaces, so everything still runs, including the tests.
    
    However, the program is using the new IConfiguration interface, and the rules and analyzer are still using the old IRuleParameterConfig interface.
    
    To defer having to refactor the analyzer, rules and rule tests, we can temporarily create an adapter class that translates between IConfiguration and IRuleParameterConfig.

--- Exercise/Exercise/IRuleParameterConfig.cs
+++ Exercise/Exercise/IRuleParameterConfig.cs
@@ -4,4 +4,25 @@
     {
         int GetRuleParam(string key);
     }
+
+    // HACK - to avoid breaking all of the rules and tests, we could add
+    // an adapter class that translates between the new IConfiguration class
+    // and the old IRuleParameterConfig class.
+    //
+    // That way we don't have to refactor everything at once; we can do it in
+    // a number of smaller PRs, rather than in one big bang.
+    public class ConfigToRuleParameterConfigAdapter : IRuleParameterConfig
+    {
+        private readonly IConfiguration configuration;
+
+        public ConfigToRuleParameterConfigAdapter(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public int GetRuleParam(string key)
+        {
+            return configuration.Rules.FirstOrDefault(x => x.RuleId == key)?.RuleParam ?? 0;
+        }
+    }
 }
--- Exercise/Exercise/Program.cs
+++ Exercise/Exercise/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Exercise.Rules;
-using Newtonsoft.Json;
 
 namespace Exercise
 {
@@ -9,24 +8,23 @@ namespace Exercise
         {
             DisplayWelcomeText();
 
-            var input = GetInput(args);
+            var rules = GetAvailableRules();
+            var configProvider = GetConfigProvider(args, rules);
+            var config = configProvider.GetConfiguration();
 
-            var filePath = input.GetPathForFileToAnalyze();
+            var filePath = config.FileToAnalyze;
             var fileContent = GetFileContent(filePath);
             var file = new File(filePath, fileContent);
 
-            var rules = GetAvailableRules();
-            var rulesToExecute = input.GetRulesToExecute(rules);
-            var ruleParameterConfig = input.GetRuleParmParameterConfig(rulesToExecute);
-
-            var result = Analyzer.Analyze(file, rulesToExecute, ruleParameterConfig);
+            var adapter = new ConfigToRuleParameterConfigAdapter(config);
+            var result = Analyzer.Analyze(file, rules, adapter);
 
             PrintResult(result);
             Console.Write("Press any key to close App");
             Console.ReadKey();
         }
 
-        private static IInput GetInput(string[] args)
+        private static IConfigProvider GetConfigProvider(string[] args, List<IRule> availableRules)
         {
             if (args.Length != 0)
             {
@@ -37,12 +35,12 @@ namespace Exercise
                     throw new Exception("Json File at path: " + args[0] + " was not found.");
                 }
 
-                var inputFromJson = new InputFromJson(args[0]);
+                var inputFromJson = new ConfigProviderJson(args[0], availableRules);
 
                 return inputFromJson;
             }
 
-            return new InputFromUser();
+            return new ConfigProviderUser(availableRules);
         }
 
         private static string[] GetFileContent(string filePath)
