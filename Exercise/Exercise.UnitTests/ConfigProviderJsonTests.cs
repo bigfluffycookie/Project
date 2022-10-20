9ff510becab3af23e254eec81b45437b62381447
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-10-20 14:30:22 +0200

    Added more tests

--- Exercise/Exercise.UnitTests/ConfigProviderJsonTests.cs
+++ Exercise/Exercise.UnitTests/ConfigProviderJsonTests.cs
@@ -11,7 +11,7 @@ public class ConfigProviderJsonTests
     public void GetConfiguration_JsonWithOneRule_ReturnsConfigWithOneRule()
     {
         var path = "path.json";
-        var fileContent = "{\r\n  \"fileToAnalyze\": \"path\",\r\n  \"rules\":{\r\n      \"ruleID\" : []\r\n   }\r\n}";
+        var fileContent = "{\r\n  \"fileToAnalyze\": \"path\",\r\n  \"rules\":{\r\n      \"ruleID\" : [0]\r\n   }\r\n}";
         var fileSystem = new Mock<IFileSystem>();
         fileSystem.Setup(p => p.File.ReadAllText(path)).Returns(fileContent);
 
@@ -19,7 +19,23 @@ public class ConfigProviderJsonTests
         var config = configProvider.GetConfiguration();
 
         Assert.AreEqual(1, config.Rules.Count());
+        Assert.AreEqual(0, config.Rules.First().RuleParam);
+        Assert.AreEqual("ruleID", config.Rules.First().RuleId);
         Assert.AreEqual("path", config.FileToAnalyze);
+    }
+
+    [TestMethod]
+    public void GetConfiguration_JsonWithNoRule_ReturnsConfigWithNoRules()
+    {
+        var path = "path.json";
+        var fileContent = "{\r\n  \"fileToAnalyze\": \"path\",\r\n  \"rules\":{\r\n   }\r\n}";
+        var fileSystem = new Mock<IFileSystem>();
+        fileSystem.Setup(p => p.File.ReadAllText(path)).Returns(fileContent);
+
+        var configProvider = new ConfigProviderJson(fileSystem.Object, path);
+        var config = configProvider.GetConfiguration();
+
+        Assert.AreEqual(0, config.Rules.Count());
     }
 
     [TestMethod]
