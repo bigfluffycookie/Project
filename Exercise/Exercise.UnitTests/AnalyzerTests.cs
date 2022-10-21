0dca2903474d8080ffca57940b0ad8c8c9d9dc87
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-10-21 09:41:28 +0200

    Fix Analyzer and InputValidatorTests

--- Exercise/Exercise.UnitTests/AnalyzerTests.cs
+++ Exercise/Exercise.UnitTests/AnalyzerTests.cs
@@ -22,9 +22,12 @@ namespace Exercise.UnitTests
         {
             var file = Mock.Of<IFile>();
             var issue = Mock.Of<IIssue>();
-            var rule = new Mock<IRule>();
-            var configuration = Mock.Of<IConfiguration>();
-            var ruleConfig = Mock.Of<IRuleConfig>();
+            var id = "id";
+
+            var ruleConfig = CreateRuleConfigWithId(id);
+            var configuration = CreateConfigurationWithRuleConfigs(new List<IRuleConfig>{ruleConfig});
+
+            var rule = CreateRuleWithId(id);
             rule.Setup(p => p.Execute(file, ruleConfig)).Returns(new List<IIssue> { issue });
 
             var result = Analyzer.Analyze(file,
@@ -39,9 +42,11 @@ namespace Exercise.UnitTests
         public void Analyze_OneRule_HasNoIssues_ReturnsEmptyList()
         {
             var file = Mock.Of<IFile>();
-            var configuration = Mock.Of<IConfiguration>();
-            var ruleConfig = Mock.Of<IRuleConfig>();
-            var rule = new Mock<IRule>();
+            var id = "id";
+            var ruleConfig = CreateRuleConfigWithId(id);
+            var configuration = CreateConfigurationWithRuleConfigs(new List<IRuleConfig> { ruleConfig });
+
+            var rule = CreateRuleWithId(id);
             rule.Setup(p => p.Execute(file, ruleConfig)).Returns(new List<IIssue>());
 
             var result = Analyzer.Analyze(file,
@@ -55,14 +60,19 @@ namespace Exercise.UnitTests
         public void Analyze_TwoRules_OneRuleHasIssue_ReturnsOneIssue()
         {
             var file = Mock.Of<IFile>();
-            var configuration = Mock.Of<IConfiguration>();
-            var ruleConfig = Mock.Of<IRuleConfig>();
-            var ruleOne = new Mock<IRule>();
-            ruleOne.Setup(p => p.Execute(file, ruleConfig)).Returns(new List<IIssue>());
+
+            var idOne = "1";
+            var ruleConfigOne = CreateRuleConfigWithId(idOne);
+            var ruleOne = CreateRuleWithId(idOne);
+            ruleOne.Setup(p => p.Execute(file, ruleConfigOne)).Returns(new List<IIssue>());
 
             var issue = Mock.Of<IIssue>();
-            var ruleTwo = new Mock<IRule>();
-            ruleTwo.Setup(p => p.Execute(file, ruleConfig)).Returns(new List<IIssue> { issue });
+            var idTwo = "2";
+            var ruleConfigTwo = CreateRuleConfigWithId(idTwo);
+            var ruleTwo = CreateRuleWithId(idTwo);
+            ruleTwo.Setup(p => p.Execute(file, ruleConfigTwo)).Returns(new List<IIssue> { issue });
+
+            var configuration = CreateConfigurationWithRuleConfigs(new List<IRuleConfig>{ruleConfigOne, ruleConfigTwo});
 
             var result = Analyzer.Analyze(file,
                                           new List<IRule> { ruleOne.Object, ruleTwo.Object },
@@ -70,6 +80,30 @@ namespace Exercise.UnitTests
 
             Assert.AreEqual(1, result.Count);
             Assert.AreEqual(issue, result[0]);
+        }
+
+        private static Mock<IRule> CreateRuleWithId(string id)
+        {
+            var rule = new Mock<IRule>();
+            rule.Setup(p => p.RuleId).Returns(id);
+
+            return rule;
+        }
+
+        private static IRuleConfig CreateRuleConfigWithId(string id)
+        {
+            var ruleConfig = new Mock<IRuleConfig>();
+            ruleConfig.Setup(p => p.RuleId).Returns(id);
+
+            return ruleConfig.Object;
+        }
+
+        private static IConfiguration CreateConfigurationWithRuleConfigs(IEnumerable<IRuleConfig> ruleConfigs)
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(p => p.Rules).Returns(ruleConfigs);
+
+            return configuration.Object;
         }
     }
 }
