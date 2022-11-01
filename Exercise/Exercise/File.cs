d1474d8bbf504b0efb9f148ff75858c3dc3abf8a
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-11-01 10:30:47 +0100

    Change file class to be responsible for loading in the file content instead of the Program class.

--- Exercise/Exercise/File.cs
+++ Exercise/Exercise/File.cs
@@ -1,14 +1,39 @@
-﻿namespace Exercise
+﻿using System.IO;
+using System;
+using System.IO.Abstractions;
+
+namespace Exercise
 {
     public class File : IFile
     {
         public string FilePath { get; }
         public string[] FileContent { get; }
 
-        public File(string filePath, string[] fileContent)
+        public File(string filePath) : this(filePath,new FileSystem()) { }
+
+        public File(string filePath, IFileSystem fileSystem)
         {
             FilePath = filePath;
-            FileContent = fileContent;
+            FileContent = GetFileContent(filePath, fileSystem);
+        }
+
+        private string[] GetFileContent(string filePath, IFileSystem fileSystem)
+        {
+            var fileContent = Array.Empty<string>();
+
+            try
+            {
+                fileContent = fileSystem.File.ReadAllLines(filePath);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("File could not be read with error message: " + e.Message);
+                Console.Write("Press any key to close App");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            return fileContent;
         }
     }
 }
