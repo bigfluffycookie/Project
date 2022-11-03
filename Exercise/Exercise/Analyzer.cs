c16d3ee61ccee5535f606e06144d5abfb5aa8acc
Leyla Büchel <leyla.buechel@sonarsource.com>
2022-11-03 15:55:43 +0100

    fix importMany issue

--- Exercise/Exercise/Analyzer.cs
+++ Exercise/Exercise/Analyzer.cs
@@ -1,12 +1,13 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
 using Exercise.Rules;
+using System.Collections.Immutable;
 
 namespace Exercise
 {
     public class Analyzer
     {
-        public static List<IIssue> Analyze(IFile file, IEnumerable<IRule> rules, IConfiguration configuration)
+        public static List<IIssue> Analyze(IFile file, ImmutableArray<IRule> rules, IConfiguration configuration)
         {
             var result = new List<IIssue>();
 
